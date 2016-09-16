19f2ff8497c0ae5ccb4873e9d9fe462b46b6d78c
Stino <Linksonder@gmail.com>
2016-09-16 16:37:42 +0200

    init

---
+++ DemoUr/DemoUr.Console/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoUr.Console2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var game = new Game();
+
+            var playing = true;
+
+            while (playing)
+            {
+                Console.ReadLine();
+                game.Next();
+                ShowField(game.Board.StartWhite);
+
+            }
+        }
+
+        public static void ShowField(Field f)
+        {
+            var theString = "[";
+
+            if(f.Piece != null)
+            {
+                theString += "x";
+            }
+            else
+            {
+                theString += " ";
+            }
+
+            theString += "]";
+            Console.Write(theString);
+
+
+            if (f.NextField != null)
+            {
+                ShowField(f.NextField);
+            }
+
+           
+        }
+    }
+}
---
+++ DemoUr/DemoUr.ModelLib/GeneratedCode/Split.cs
@@ -0,0 +1,17 @@
+﻿internal class Split : Field
+{
+    public Field AlternativeNext { get; set; }
+
+
+    public override Field NextField
+    {
+        get
+        {
+            //op basis van kleur de juiste nextField teruggeven
+            return null;
+        }
+
+    }
+
+
+}
